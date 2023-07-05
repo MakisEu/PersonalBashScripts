d34d258a63ea0a3c3128689218b20e900d42816c
MakisEu <euaggelinosmakis@gmail.com>
2023-07-05 20:40:01 +0300

    Added Script to play a video using cutom shortcut by copying url from my RSS reader.

---
+++ PlayRSSVideo.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#read -p "Enter the URL: " url
+
+url=$(xclip -selection c -o)
+echo $url
+# Extract domain name from the URL
+#domain=$(echo "$url" | awk -F[/:] '{print $4}')
+is_yt=$(echo "$url" | cut -c 1-24)
+# Check if domain belongs to youtube.com
+if [ "$is_yt" == "https://www.youtube.com/" ]; then
+#echo "The URL belongs to youtube.com."
+#yt-dlp -f 18 $url --sub-lang en -o - | mpv - -force-seekable=yes
+mpv $url
+else
+echo "The URL does not belong to youtube.com."
+fi
+
+
+
+
+
