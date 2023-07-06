7133c7c963613f34f76683200133fed13025f34a
MakisEu <euaggelinosmakis@gmail.com>
2023-07-06 03:50:30 +0300

    Added BashHistorySaver and impoved PlayRSSVideo

--- PlayRSSVideo.sh
+++ PlayRSSVideo.sh
@@ -3,7 +3,7 @@
 #read -p "Enter the URL: " url
 
 url=$(xclip -selection c -o)
-echo $url
+#echo $url
 # Extract domain name from the URL
 #domain=$(echo "$url" | awk -F[/:] '{print $4}')
 is_yt=$(echo "$url" | cut -c 1-24)
@@ -11,7 +11,7 @@ is_yt=$(echo "$url" | cut -c 1-24)
 if [ "$is_yt" == "https://www.youtube.com/" ]; then
 #echo "The URL belongs to youtube.com."
 #yt-dlp -f 18 $url --sub-lang en -o - | mpv - -force-seekable=yes
-mpv $url
+mpv $url && echo "$url">> Documents/Histories/Youtube.txt
 else
 echo "The URL does not belong to youtube.com."
 fi
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Scripts
 A repo to store the bash script I created to make my life easier
+
+Scripts:
+
+        # :backhand_index_pointing_right: PlayRSSVideo: Play a youtube video using yt-dlp and mpv from a link and add link to history file.
+        
+        # :backhand_index_pointing_right: BashHistorySaver: Save terminal history permanently into a file and clear bash_history to speed up terminal
